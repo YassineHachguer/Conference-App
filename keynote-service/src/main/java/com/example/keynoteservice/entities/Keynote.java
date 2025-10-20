e1cea34b85c165e0f03d24aa4a64be72a563dfec
YassineHachguer <yassine.hachguer05@gmail.com>
2025-10-20 09:52:31 +0100

    Implémentation de Keynote-service

---
+++ keynote-service/src/main/java/com/example/keynoteservice/entities/Keynote.java
@@ -0,0 +1,25 @@
+package com.example.keynoteservice.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Keynote {
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    private String nom;
+    private String prenom;
+    private String email;
+    private String fonction;
+}
---
+++ keynote-service/src/main/java/com/example/keynoteservice/repository/KeynoteRepository.java
@@ -0,0 +1,11 @@
+package com.example.keynoteservice.repository;
+
+import com.example.keynoteservice.entities.Keynote;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+
+import java.util.UUID;
+
+@RepositoryRestResource
+public interface KeynoteRepository extends JpaRepository<Keynote, UUID> {
+}
